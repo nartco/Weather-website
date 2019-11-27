652e622a8ca8f9e529ab73bc5dde19d1150da514
nartco <sebastien.thuillier@hotmail.com>
2019-11-27 16:56:23 +0100

    change syntax error of address

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ app.get('/help', (req, res) => {
 
 app.get('/weather', (req, res) => {
 
-    if(!req.query.adress){
+    if(!req.query.address){
         return res.send({
             error: 'you must provide an adress'
         })
