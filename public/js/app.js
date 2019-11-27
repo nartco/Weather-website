4ea9efe9df8f85297dfce92fc8e4f674a5978d3e
nartco <sebastien.thuillier@hotmail.com>
2019-11-27 16:54:04 +0100

    change syntax error of address

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value
 
-    fetch(`/weather?adress=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
         if(data.error){
             messageOne.textContent = data.error
